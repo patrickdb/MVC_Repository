17375e04c4ebc28ffda083a06e95b393600c19a6
Patrick de Beer <p.debeer@fontys.nl>
2017-11-17 09:37:14 +0100

    Update missing  MVCREpo oefening

---
+++ WebApplication2/Models/AdresModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class AdresModel
+    {
+        public string Straat { get; }
+
+        public AdresModel(string straat)
+        {
+            this.Straat = straat;
+        }
+    }
+}
--- WebApplication2/Repository/PersoonSQLContext.cs
+++ WebApplication2/Repository/PersoonSQLContext.cs
@@ -1,20 +1,39 @@
-﻿using System;
+﻿using MVCRepo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace MVCRepo.Repository
 {
+    /**
+     * Fake database class om een persoon op te slaan en 
+     * later weer op te halen.
+     */
     public class PersoonSQLContext : IPersoonContext
     {
-        public void AddPerson()
+        PersoonModel pm;
+
+        public void slaOp(PersoonModel persoon)
         {
-            throw new NotImplementedException();
+            pm = persoon;
         }
 
-        public void GetFirstPerson()
-        {
-            throw new NotImplementedException();
+        // Laad de persoon die is opgeslagen
+        // Een speciaal geval is persoon met bsn 1712556789, die zal Pietje genoemd moeten worden
+        public PersoonModel laadPersoon(int bsn)
+        {            
+            if (pm == null)
+            { 
+               pm = new PersoonModel();
+            }
+
+            if (bsn == 1712556789)
+            {
+                pm.Naam = "Pietje";
+            }
+
+            return pm;
         }
     }
 }
